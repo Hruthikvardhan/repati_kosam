081e9d9c60a427ea255fadd54bac482e24ae8c1d
Hruthik Vardhan <hruthikwarrior@gmail.com>
2025-09-22 23:14:33 +0530

    updated python code

---
+++ day 1/1.py
@@ -0,0 +1,6 @@
+# Write a program to check whether a given number is even or odd.
+num = int(input("Enter any number"))
+if num%2==0:
+    print("Even")
+else:       
+    print("Odd")
---
+++ day 1/2.py
@@ -0,0 +1,11 @@
+#Write a program to find the largest of three numbers entered by the user.
+
+num1 = int(input("enter 1 st num"))
+num2 = int(input("enter 2 nd num"))
+num3 = int(input("enter 3 rd num"))
+if((num1 > num2) and  (num1 > num3)):
+    print("num 1 largest")
+elif((num2 > num1) and (num2 > num3)):
+    print("num 2 largest")
+else :
+    print("num 3 is largest")
